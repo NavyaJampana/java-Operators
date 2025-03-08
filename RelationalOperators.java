e3d55663c664911334effb2b48b0c3f29cebfebd
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 16:31:06 +0530

    Add files via upload

---
+++ RelationalOperators.java
@@ -0,0 +1,11 @@
+package Operator;
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int x = 15, y = 10;
+
+        System.out.println("x < y: " + (x < y));
+        System.out.println("x <= y: " + (x <= y));
+        System.out.println("x > y: " + (x > y));
+        System.out.println("x >= y: " + (x >= y));
+    }
+}
