c2f761a0d091651a9a166dfc7a8b7ea67e017532
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 16:30:48 +0530

    Add files via upload

---
+++ LogicalOperators.java
@@ -0,0 +1,11 @@
+package Operator;
+public class LogicalOperators {
+    public static void main(String[] args) {
+        boolean a = true, b = false;
+
+        System.out.println("Logical AND (a && b): " + (a && b));
+        System.out.println("Logical OR (a || b): " + (a || b));
+        System.out.println("Logical NOT (!a): " + (!a));
+        System.out.println("Logical NOT (!b): " + (!b));
+    }
+}
