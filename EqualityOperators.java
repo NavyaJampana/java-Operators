23c77929da34b59a81d0eaf1863fdbae1704b602
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 16:30:05 +0530

    Add files via upload

---
+++ EqualityOperators.java
@@ -0,0 +1,9 @@
+package Operator;
+public class EqualityOperators {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        System.out.println("a == b: " + (a == b)); // Equal to
+        System.out.println("a != b: " + (a != b)); // Not equal to
+    }
+}
